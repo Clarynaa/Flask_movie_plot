a98036f08e147c2a99ac839021a9bd82f185e266
Clarynaa <32001815+Clarynaa@users.noreply.github.com>
2018-11-26 16:44:07 -0600

    Finished, unpolished product complete

--- main.py
+++ main.py
@@ -62,10 +62,10 @@ def index():
         db.session.commit()
     if request.args.get('page'):
         page = int(request.args.get('page'))
-        movies = Movie.query.order_by(Movie.id.desc()).paginate(page = page, per_page=20)
+        movies = Movie.query.order_by(Movie.id.desc()).paginate(page = page, per_page=20).items
         return render_template('home.html', movies=movies.items)
     else:
-        movies = Movie.query.order_by(Movie.id.desc()).all()
+        movies = Movie.query.order_by(Movie.id.desc()).paginate(page = 1, per_page=20).items
         return render_template('home.html', movies=movies)
 
 @app.route('/edit', methods=['POST'])
@@ -75,6 +75,46 @@ def edit():
     return render_template('edit.html', movie = movie)
     #TODO: Add editing.  edit button on previous page links to a specific item.  Which is then displayed here in editable format, then updated via sql on submission.   ALso allow adding
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method=='POST':
+        new_yr = request.form['yr']
+        new_title = request.form['title']
+        new_origin = request.form['origin']
+        new_director = request.form['director']
+        new_cast = request.form['cast']
+        new_genre = request.form['genre']
+        new_wiki = request.form['wiki']
+        new_plot= request.form['plot']
+        movie = Movie(new_yr, new_title, new_origin, new_director, new_cast, new_genre, new_wiki, new_plot)
+
+        db.session.add(movie)
+        db.session.commit()
+        return render_template('home.html', movies = Movie.query.order_by(Movie.id.desc()).paginate(page = 1, per_page=20).items)
+    else:
+        return render_template('add.html')
+
+@app.route('/import')
+def imprt():
+    count = 0
+    db.create_all()
+    with open('movie_plots.csv', encoding='utf-8') as mycsv:
+        reader = csv.DictReader(mycsv,  quotechar='"')
+        for row in reader:
+            count += 1
+            movie = Movie(row['Release Year'], row['Title'], row['Origin/Ethnicity'], row['Director'], row['Cast'], row['Genre'], row['Wiki Page'], row['Plot'])
+            db.session.add(movie)
+            db.session.commit()
+    return render_template('confirm.html', message='Deletion of ' + count + ' items successful')
+        
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    mov_id = request.form['delete']
+    movie = Movie.query.get(mov_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return render_template('confirm.html', message="Deletion successful.")
 
 if __name__ == "__main__":
     app.run()
